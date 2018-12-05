7c16328a70768fb6b6fb6e61f4d17f8f42b05ac6
Avinash <avinashb97@gmail.com>
2018-12-06 00:41:35 +0530

    Adds: Contact Model

---
+++ src/models/contacts.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const ContactSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String
+  },
+  phone: {
+    dialCode: String,
+    number: Number,
+    required: true
+  },
+  lastUpdateAt: { type: Date, default: Date.now }
+});
+
+ContactSchema.post('save', () => {
+  const data = this;
+  data.lastUpdateAt = new Date();
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
